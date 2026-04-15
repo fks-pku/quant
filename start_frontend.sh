0c61fae5a54a79db95f84602b608bfaed901ab77
fks <fkscreed@gmail.com>
2026-04-15 15:56:56 +0800

    phase1

--- start_frontend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "/Users/bytedance/Desktop/vk/quant/.vibe-kanban-workspaces/cf89-update-the-ui/quant/frontend"
-export PATH="/Users/bytedance/.nvm/versions/node/v22.22.0/bin:$PATH"
-npm start &
