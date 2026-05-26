36c3750999ea0d22dfc814b29b49ffd663e8f1c0
fkscreed <fkscreed@gmail.com>
2026-05-27 00:16:01 +0800

    feat: export QMTBroker from brokers package

--- quant/infrastructure/execution/brokers/__init__.py
+++ quant/infrastructure/execution/brokers/__init__.py
@@ -2,5 +2,6 @@
 
 from quant.infrastructure.execution.brokers.base import BrokerAdapter
 from quant.infrastructure.execution.brokers.paper import PaperBroker
+from quant.infrastructure.execution.brokers.qmt import QMTBroker
 
-__all__ = ["BrokerAdapter", "PaperBroker"]
+__all__ = ["BrokerAdapter", "PaperBroker", "QMTBroker"]
